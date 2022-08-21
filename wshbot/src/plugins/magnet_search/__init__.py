eb85d5461b895db96d0190f69964cbc81ed58105
DengSchoo <1425123490@qq.com>
2022-08-21 10:20:22 +0800

    message类型修正

--- wshbot/src/plugins/magnet_search/__init__.py
+++ wshbot/src/plugins/magnet_search/__init__.py
@@ -2,7 +2,7 @@ from nonebot import on_command
 from nonebot.adapters.onebot.v11 import Bot, Event
 from nonebot.rule import to_me
 from nonebot.matcher import Matcher
-from nonebot.adapters import Message
+from nonebot.adapters.onebot.v11.message import Message
 from nonebot.params import Arg, CommandArg, ArgPlainText
 
 from . import warden as wd
