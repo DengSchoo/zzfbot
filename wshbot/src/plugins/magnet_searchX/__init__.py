434ddfb8f3c9a9724744105f8f9cb5b952542a94
DengSchoo <1425123490@qq.com>
2022-09-03 22:21:54 +0800

    v0.2 mag_searchX：warden bug fix

--- wshbot/src/plugins/magnet_searchX/__init__.py
+++ wshbot/src/plugins/magnet_searchX/__init__.py
@@ -14,8 +14,8 @@ mag_search = on_command("mag_searchX", aliases={"全资源磁力搜索"}, priori
 @mag_search.handle()
 async def handle_first_receive(bot: Bot, event: Event):
     user_id = int(event.get_user_id())
-    if not Warden_User(str(user_id)):
-        return await mag_search.send(Message(f'[CQ:at,qq={int(user_id)}]' +  '找wsh申请权限~'))
+    if not Warden_User(int(user_id)):
+        return await mag_search.send(Message(f'[CQ:at,qq={int(user_id)}]' + '找wsh申请权限~'))
     message = event.get_message()
     key_word = str(message).strip().split(' ')[1].encode('utf-8')
     res = sp.search_res(key_word)
