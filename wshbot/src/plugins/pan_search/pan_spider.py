02d2094249877f9a513b8ee1fe8e12f837e563f0
DengSchoo <1425123490@qq.com>
2023-03-26 21:59:21 +0800

    v0.3 spider optimize

--- wshbot/src/plugins/pan_search/pan_spider.py
+++ wshbot/src/plugins/pan_search/pan_spider.py
@@ -1,4 +1,5 @@
 import re
+#coding=utf-8
 
 import requests
 from lxml import etree
@@ -46,8 +47,28 @@ search_urls = {
 
 headers = {
     'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36 Edg/105.0.1343.27',
-    'cookie': 'test_cookie=CheckForPermission; expires=Fri, 09-Sep-2022 10:08:08 GMT; path=/; domain=.doubleclick.net; Secure; HttpOnly; SameSite=none'
+    'cookie': '_ga=GA1.1.481167973.1667014847; __gads=ID=216fae9a67a9d5ee-224095a038d900f4:T=1673356318:RT=1673356318:S=ALNI_MYnvxvlAUNHLEe37h13I-CkWhBxrQ; Hm_lvt_02f69e0ba673e328ef49b5fb98dd4601=1677897985,1677936657,1678621279,1679834163; _bid=f34e039111870ec62cac42f73be95917; __gpi=UID=00000ba14569e8e7:T=1673356318:RT=1679834163:S=ALNI_Mb3QzLchd5HxoifHPyvpsdmqPYwxA; _ga_NYNC791BP2=GS1.1.1679834163.28.1.1679834360.0.0.0; _ga_0B2NFC7Z09=GS1.1.1679834163.26.1.1679834360.17.0.0; Hm_lpvt_02f69e0ba673e328ef49b5fb98dd4601=1679834361',
+    'referer': 'https://www.alipansou.com/s/Ye8OmwQDjxXEOqMNRb3HnAZfrxVbP'
 }
+
+# headers = {
+# 'Host': 'www.alipansou.com',
+# 'Connection': 'keep-alive',
+# 'sec-ch-ua: '"Microsoft Edge";v="111", "Not(A:Brand";v="8", "Chromium";v="111"',
+# 'sec-ch-ua-platform': "Windows",
+# 'sec-ch-ua-mobile': '?0'.
+# 'Upgrade-Insecure-Requests': 1
+# 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36 Edg/111.0.1661.54,
+# 'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+# 'Sec-Fetch-Site': same-origin
+# 'Sec-Fetch-Mode': navigate
+# 'Sec-Fetch-User': ?1
+# 'Sec-Fetch-Dest': document
+# 'Referer': https://www.alipansou.com/s/AdS6dklGcuwOqXtoMoWMs0VqAOPai
+# 'Accept-Language: zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7,en-GB;q=0.6
+# 'Cookie': '_ga=G'A1.1.481167973.1667014847; __gads=ID=216fae9a67a9d5ee-224095a038d900f4:T=1673356318:RT=1673356318:S=ALNI_MYnvxvlAUNHLEe37h13I-CkWhBxrQ; Hm_lvt_02f69e0ba673e328ef49b5fb98dd4601=1677897985,1677936657,1678621279,1679834163; _bid=f34e039111870ec62cac42f73be95917; __gpi=UID=00000ba14569e8e7:T=1673356318:RT=1679834163:S=ALNI_Mb3QzLchd5HxoifHPyvpsdmqPYwxA; _ga_NYNC791BP2=GS1.1.1679834163.28.1.1679834360.0.0.0; _ga_0B2NFC7Z09=GS1.1.1679834163.26.1.1679834360.17.0.0; Hm_lpvt_02f69e0ba673e328ef49b5fb98dd4601=1679834361
+# 'Accept-Encoding': 'gzip, deflate'
+# }
 
 fir_sea_xpath = '/html/body/div/div[1]'
 
@@ -56,7 +77,7 @@ first_max_num = 3
 
 def get_list(url: str, keyword: str) -> list:
     #result = requests.get(url + search_url_part + keyword, headers=headers, proxies=proxies)
-    result = requests.get(url + search_url_part + keyword, headers=headers)
+    result = requests.get(url + search_url_part + keyword, headers=headers, verify=False)
     tree = etree.HTML(result.text)
     #print("text" + result.text + "\n")
     
@@ -82,9 +103,9 @@ tab_str = tab_str + tab_str
 
 
 def get_sec_res(url: str) -> str:
-    result = requests.get(url, headers=headers)
+    result = requests.get(url, headers=headers, verify=False)
     tree = etree.HTML(result.text)
-    print(result.text)
+    #print(result.text)
     root = tree.xpath('/html/body/div/div[1]')[0]
 
     ret_msg = ''
@@ -100,10 +121,14 @@ def get_sec_res(url: str) -> str:
             meta_val = str(meta.xpath('./van-col/van-cell/text()')[0]).strip()
         ret_msg += f'{tab_str + meta_name + "：" + meta_val}\n'
     js_shell = tree.xpath('/html/body/script[3]/text()')[0]
-    url = tab_str + '资源链接：' + process_js_shell(js_shell) + '\n'
-    if 'aliyun' in url:
-        url = url.replace('\\', '')
+    cv = process_js_shell(js_shell)
+    headers_tmp = headers
+    headers_tmp['referer'] = aliyun_url + cv
+    TMP_URL = requests.get(aliyun_url + cv, headers=headers_tmp, verify=False).url
+    url = tab_str + '资源链接：' + TMP_URL + '\n'
     return ret_msg + url
+
+
 
 
 def process_js_shell(js_shell: str) -> str:
@@ -133,7 +158,7 @@ def get_search_type_op(key: str):
 
 def pan_res_search(keyword: str):
     splits = keyword.strip().split(' ')
-    print(''.join(splits))
+    #print(''.join(splits))
     lenth = len(splits)
     search_key = ''
     if lenth == 1 or lenth == 2:
@@ -156,4 +181,4 @@ def pan_res_search(keyword: str):
 
 
 
-#print(pan_res_search('ps al 黑化律师'))
+print(pan_res_search('ps al 黑化律师'))
